569942cf92cf8613b672796ea88e9eb5ca7e3922
learngitbasics <98641663+learngitbasics@users.noreply.github.com>
2022-01-29 18:00:28 +0530

    Add files via upload
    Example of for loop

---
+++ ForLoop.java
@@ -0,0 +1,23 @@
+package package1;
+
+
+/**
+ * 5
+ * 4
+ * 0
+ * 1
+ * 6
+ */
+
+public class ForLoop {
+
+    public static void main(String[] args) {
+        int i=0;
+        for(; i< 5; i++){
+            System.out.println(i);
+        }
+
+        System.out.println("value of i::"+i);
+
+    }
+}
